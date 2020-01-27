fa9faa9b0f50d325fdbe3f5bfeb7bb40b3961cf9
pritoranta <60341214+pritoranta@users.noreply.github.com>
2020-01-27 12:23:46 +0200

    Update fibonacci.hs
    revised

--- fibonacci.hs
+++ fibonacci.hs
@@ -1,7 +1,12 @@
--- WARNING! very ineffective
--- the nth element of the fibonacci sequence
-fib :: Int -> Int
-fib n = case n of
- 1 -> 1
- 2 -> 1
- x -> (fib (x-1))+(fib (x-2))
+-- the Nth element of the Fibonacci sequence
+-- upper limit 5611 is set because of the limits of type Int
+fibN :: Int -> Maybe Int
+fibN n
+    | n < 0 || n > 5611 = Nothing
+    | otherwise = Just (fibSequence !! n)
+
+-- USE CAREFULLY!
+-- use fibN if you don't need a list
+-- Fibonacci Sequence as an infinite list
+fibSequence :: [Int]
+fibSequence = 0 : 1 : zipWith (+) fibSequence (tail fibSequence)
